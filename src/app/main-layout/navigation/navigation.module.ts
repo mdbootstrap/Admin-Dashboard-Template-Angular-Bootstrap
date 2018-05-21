dbc95c6ca5bc22728872239983f95de50babe891
Damian <d.gemza@mdbootstrap.com>
2018-05-21 11:05:22 +0200

    6.1.1 Release

--- src/app/main-layout/navigation/navigation.module.ts
+++ src/app/main-layout/navigation/navigation.module.ts
@@ -1,4 +1,4 @@
-import { MDBBootstrapModule } from './../../typescripts/free/index';
+import { MDBBootstrapModule } from './../../../../angular-bootstrap-md';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
