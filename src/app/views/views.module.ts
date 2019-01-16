52dbb0c1662a6379cec614f2cd90e1ca9f4f1e59
Damian Gemza <d.gemza@mdbootstrap.com>
2019-01-16 08:17:06 +0100

    Added compatibility with Font Awesome 5

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -12,18 +12,10 @@ import { SharedModule } from '../shared/shared.module';
 import { FooterComponent } from '../main-layout/footer/footer.component';
 import { BasicTableComponent } from './tables/basic-table/basic-table.component';
 import { ModalsComponent } from './modals/modals.component';
-import { TypographyComponent } from './css/typography/typography.component';
-import { IconsComponent } from './css/icons/icons.component';
 import { Map1Component } from './maps/map1/map1.component';
 import { StatsCardComponent } from './dashboards/common/stats-card/stats-card.component';
 import { StatsCard2Component } from './dashboards/common/stats-card2/stats-card2.component';
 import { Dashboard1Component } from './dashboards/dashboard1/dashboard1.component';
-import { GridComponent } from './css/grid/grid.component';
-import { MediaObjectComponent } from './css/media-object/media-object.component';
-import { UtilitiesComponent } from './css/utilities/utilities.component';
-import { ImagesComponent } from './css/images/images.component';
-import { ColorsComponent } from './css/colors/colors.component';
-import { ShadowComponent } from './css/shadow/shadow.component';
 import { Profile1Component } from './profile/profile1/profile1.component';
 import { HelpComponent } from './help/help.component';
 
@@ -46,18 +38,10 @@ import { HelpComponent } from './help/help.component';
     FooterComponent,
     BasicTableComponent,
     ModalsComponent,
-    TypographyComponent,
-    IconsComponent,
     Map1Component,
     StatsCardComponent,
     StatsCard2Component,
     Dashboard1Component,
-    GridComponent,
-    MediaObjectComponent,
-    UtilitiesComponent,
-    ImagesComponent,
-    ColorsComponent,
-    ShadowComponent,
     Profile1Component,
     HelpComponent,
 
@@ -66,19 +50,10 @@ import { HelpComponent } from './help/help.component';
     FooterComponent,
     BasicTableComponent,
     ModalsComponent,
-    TypographyComponent,
-    IconsComponent,
     Map1Component,
     StatsCardComponent,
     StatsCard2Component,    
-    Dashboard1Component,
-    GridComponent,
-    MediaObjectComponent,
-    UtilitiesComponent,
-    ImagesComponent,
-    ColorsComponent,
-    ShadowComponent,
-
+    Dashboard1Component
   ],
   schemas: [NO_ERRORS_SCHEMA]
 })
