67fc7df56b3107ed43cab31253c66977f3992bd7
a.idzikowski <a.idzikowski@mdbootstrap.com>
2020-09-16 16:35:53 +0200

    Update Angular version to v10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,50 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Route } from '@angular/router';
 import { AppComponent } from './app.component';
-import { AppRoutes } from './app.routes.service';
 
 import { ViewsModule } from './views/views.module';
 import { SharedModule } from './shared/shared.module';
 import { ErrorModule } from './views/errors/error.module';
 
+import { Map1Component } from './views/maps/map1/map1.component';
+import { ModalsComponent } from './views/modals/modals.component';
+import { BasicTableComponent } from './views/tables/basic-table/basic-table.component';
+import { Profile1Component } from './views/profile/profile1/profile1.component';
+import { NotFoundComponent } from './views/errors/not-found/not-found.component';
+import { Dashboard1Component } from './views/dashboards/dashboard1/dashboard1.component';
+
 // main layout
 import { NavigationModule } from './main-layout/navigation/navigation.module';
+
+const routes: Route[] = [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboards/v1' },
+  { path: 'dashboards', children:
+    [
+      { path: 'v1', component: Dashboard1Component },
+    ]
+  },
+  { path: 'profiles', children:
+    [
+      { path: 'profile1', component: Profile1Component },
+    ]
+  },
+  { path: 'tables', children:
+    [
+      { path: 'table1', component: BasicTableComponent },
+    ]
+  },
+  { path: 'maps', children:
+    [
+      { path: 'map1', component: Map1Component},
+    ]
+  },
+
+  { path: 'modals', component: ModalsComponent},
+  { path: '**', component: NotFoundComponent },
+
+];
 
 @NgModule({
   declarations: [
@@ -25,13 +59,12 @@ import { NavigationModule } from './main-layout/navigation/navigation.module';
     BrowserModule,
     BrowserAnimationsModule,
     NavigationModule,
-    AppRoutes,
-    RouterModule,
+    RouterModule.forRoot(routes),
     FormsModule,
     SharedModule,
     ViewsModule,
     ErrorModule,
-    FormsModule, 
+    FormsModule,
     ReactiveFormsModule
   ],
   providers: [],
